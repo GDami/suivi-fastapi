a4afda69b719c65c544afb66497acf94e442f451
Damien <d.gervy@free.fr>
2026-02-19 14:17:34 +0100

    sidebar colors make more sense

--- front/src/components/Sidebar.tsx
+++ front/src/components/Sidebar.tsx
@@ -1,19 +1,32 @@
-import { Link } from "react-router"
+import { Link, useLocation } from "react-router"
 
 const styles = {
     h2: "text-2xl font-bold mb-4",
     li: "mb-2",
+    currentLink: "text-white",
+    link: "text-gray-400 hover:text-white"
 }
 
+const navLinks = [
+    { name: "Accueil", path: "/home" },
+    { name: "Candidatures", path: "/applications" },
+    { name: "Entreprises", path: "/companies" },
+    // { name: "Actions", path: "/actions" },
+]
+
 export default function Sidebar() {
+
+    const location = useLocation();
+    
     return (
-        <aside className="w-64 min-h-screen bg-gray-800 text-white p-4">
+        <aside className="w-64 min-h-screen bg-gray-800 text-white p-4 selection:bg-white selection:text-gray-800">
             <h2 className={styles.h2}>suivi-v2</h2>
             <ul>
-                <li className={styles.li}><Link to="/home" className="hover:text-gray-400">Accueil</Link></li>
-                <li className={styles.li}><Link to="/applications" className="hover:text-gray-400">Candidatures</Link></li>
-                {/* <li className={styles.li}><Link to="/companies" className="hover:text-gray-400">Companies</Link></li>
-                <li className={styles.li}><Link to="/actions" className="hover:text-gray-400">Actions</Link></li> */}
+                {navLinks.map((link, index) => (
+                    <li key={index} className={styles.li}>
+                        <Link to={link.path} className={location.pathname === link.path ? styles.currentLink : styles.link}>{link.name}</Link>
+                    </li>
+                ))}
             </ul>
         </aside>
     )
